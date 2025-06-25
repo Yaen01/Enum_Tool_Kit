ac967860183587057c782c55aa2a3862ef38c5a9
DanaFletcher <danalynnfletcher@Outlook.com>
2025-06-24 23:20:09 -0400

    EnumTK 2.0

--- Enumtk.py
+++ Enumtk.py
@@ -1,179 +1,248 @@
-#!/usr/bin/python
-import os, sys, re
-from termcolor import colored, cprint
+#!/usr/bin/env python3
+import cmd
+import os
+import re
+from termcolor import colored
+from colorama import init, Fore, Style
 
-# Function for creating the Menu Title
-def createMenuTitle(menuName):
-    menuName = menuName.center(40, " ")
-
-    #Script Header
+# Banner
+def show_banner():
     os.system("clear")
+    init()
+    ascii_art = """
+    .-''-.  ,---.   .--.  ----  -- ,---.    ,---.,---------. .--.   .--.   
+  .'_ _   \\ |    \\  |  |.'   /  | ||    \\  /    |\\          \\|  | _/  /    
+ / ( ` )   '|  ,  \\ |  ||   |   | ||  ,  \\/  ,  | '--.  ,---'| (`` ) /     
+. (_ o _)  ||  |\\_ \\|  ||   |   | ||  |\\_   /|  |    |   \\   |(_ ()_)      
+|  (_`_)---||  |( )_\\  ||   |_ _| ||  |( )_/ |  |    :_ _:   | (_;_)   ---  
+'  \\   .---.| (_ o _)  ||   ( v ) || (_ o _) |  |    (_v_)   |  |\\ \\  |  | 
+ \\  '-'    /|  (_;_)\  ||  (_ O _)||  (_^_)  |  |   (_ O _)  |  | \\  '   / 
+  \\       / |  |    |  |\\   ( ; ) /|  |      |  |    (_^_)   |  |  \\    /  
+   \'-..-'   '--'    '--' ''-----'' '--'      '--'    '---'   '--'   ''-'   
+"""
 
-    print(colored("""
-███████ ███    ██ ██    ██ ███    ███ ████████ ██   ██ 
-██      ████   ██ ██    ██ ████  ████    ██    ██  ██  
-█████   ██ ██  ██ ██    ██ ██ ████ ██    ██    █████   
-██      ██  ██ ██ ██    ██ ██  ██  ██    ██    ██  ██  
-███████ ██   ████  ██████  ██      ██    ██    ██   ██  
-        """, 'light_magenta', attrs=['bold']))
+# Characters to color as "flower art"
+    flower_chars = set("()_*oO`v^=;")
 
-    print(colored("----------------------------------------", 'light_green', attrs=['bold']))
-    print(colored(menuName, 'light_green', attrs=['bold']))
-    print(colored("----------------------------------------", 'light_green', attrs=['bold']))
+    for line in ascii_art.splitlines():
+        colored_line = ''.join(
+            Fore.MAGENTA + char + Style.RESET_ALL if char in flower_chars else char
+            for char in line
+    )
+        print(colored_line)
+    print(colored("------------- Enumeration Toolkit -------------", 'light_green', attrs=['bold']))
 
-# Function for creating the Menu Item List
-def createMenuList(menuItems):
-    i = 0
-    print("")
-
-    for x in menuItems:
-        i+=1
-        print(colored(f"[{i}] ", 'light_cyan') + x)
-
-# Function for input validation for IP with CIDR notation
+# IP validation
 def validIp(scope):
-    # Input validation for IP address
     pattern = r'^([0-9]{1,3}\.){3}[0-9]{1,3}(\/[0-9]{1,2})?$'
     if not re.match(pattern, scope):
         return False
     ip, cidr = scope.split('/') if '/' in scope else (scope, '32')
     parts = ip.split('.')
-    if all(0 <= int(part) <= 255 for part in parts) and 0 <= int(cidr) <= 32:
-        return True
-    return False
+    return all(0 <= int(part) <= 255 for part in parts) and 0 <= int(cidr) <= 32
 
-# Function for the Main Menu
-def mainMenu():
-    while (1):
+# Main shell
+class EnumShell(cmd.Cmd):
+    prompt = colored("enumtk> ", "green")
 
-        #Menu Title
-        createMenuTitle("Main Menu")
+    def __init__(self):
+        super().__init__()
+        self.module = None
+        self.options = {}
 
-        #Menu Display
-        createMenuList(["Nmap General", "Nmap Protocol Specific", "Exit"])
+    def load_module(self, module):
+        self.module = module
+        if module == "nmap":
+            self.options = {
+                "SCAN": {"value": "", "required": True, "desc": "Scan type (discovery, stealth, all)"},
+                "IP": {"value": "", "required": True, "desc": "Target IP"},
+                "PORTS": {"value": "1-1000", "required": False, "desc": "Ports to scan"},
+                "INTERFACE": {"value": "eth0", "required": False, "desc": "Interface to use"},
+                "MISC": {"value": "", "required": False, "desc": "Whatever Torres wants here"},
+                "SAVE": {"value": "false", "required": False, "desc": "Save output (true/false)"},
+            }
+        elif module == "nmap_protocol":
+            self.options = {
+                "RHOST": {"value": "", "required": True, "desc": "Target IP"},
+                "PROTOCOL": {"value": "ftp", "required": True, "desc": "Protocol to scan (ftp/http/smtp)"},
+            }
 
-        # Menu Selection
-        x = input("\nSelect from menu above: ")
-
-        # Nmap General Conditional
-        if x == '1':
-            nmapMenu()
-
-        # Nmap Protocol Specific
-        elif x == '2':
-            print("In-Development")
-
-        elif x == '3':
-            break
-
-        else:
-            input(colored("Invalid selection, please press enter.", 'red', attrs=['bold']))
-            continue
-
-# Function for the Nmap General Menu
-def nmapMenu():
-    while (1):
-
-        #Menu Title
-        createMenuTitle("Nmap General Menu")
-
-        #Menu Display
-        createMenuList(["Nmap Discovery Scan (-sn)", "Nmap Syn-Stealth Scan (-sS)", "Nmap All Scan (-A)", "Go Back"])
-
-        # Menu Selection
-        x = input("\nSelect from menu above: ")
-
-        # Nmap Discovery Scan
-        if x == '1':
-
-            # Loop until a valid IP is entered
-            while True:
-                ipScope = input("\nEnter the scope: (Example: 192.168.1.1/24) ")
-                if validIp(ipScope):
-                    break
-                else:
-                    print("\nInvalid IP address format. Please enter a valid IP address in CIDR notation.")
-
-            saveOption = input("\nWould you like to save the results? (Y/N): ")
-
-            #Saved Discovery Scan
-            if (saveOption == 'y' or saveOption == 'Y'):
-                scanInterface = input("\nEnter the network interface name: (Example: eth0, wlan0) ")
-
-                #Runs Discovery Scan
-                aliveHosts = os.system(f"nmap -sn {ipScope} -e {scanInterface} | grep 'Nmap scan' | cut -d ' ' -f 5 > ./discoveryScan.txt")
-                savePath = os.popen(f"pwd").read()
-                input(f"\nFile was saved in {savePath}/discoveryScan.txt\n\nPress Enter to Cotinue:")
-                #Print statement for testing
-                #print("Correct if y is chosen and IP is correct")
-                break
-            
-            #Unsaved Discovery Scan
-            elif (saveOption == 'n' or saveOption == 'N'):
-                scanInterface = input("\nEnter the network interface name: (Example: eth0, wlan0) ")
-                os.system(f"nmap -sn {ipScope} -e {scanInterface}")
-                #Print statement for testing
-                #print("Correct Unsaved discovery scan")
-                break
-
-            #Invalid Save Option Input 
+    # Initial menu input handler
+    def precmd(self, line):
+        if not self.module:
+            if line == "1":
+                self.load_module("nmap")
+                print(colored("Loaded nmap", "white"))
+                return ""
+            elif line == "2":
+                self.load_module("nmap_protocol")
+                print(colored("Loaded module nmap_protocol", "white"))
+                return ""
+            elif line == "3":
+                return "exit"
             else:
-                input(colored("\nInvalid Input, please press enter.", 'red', attrs=['bold']))
-                continue
-            
-        # Syn Stealth Scan Conditional
-        elif x == '2':
-            importIP = input("\nWould you like to import an IP list? (Y/N) ")
-            
-            # Imported IP List Syn Stealth Scan
-            if (importIP == 'y' or importIP == 'Y'):
+                print(colored("[!] Invalid selection. Type 1, 2, or 3.", "red"))
+                return ""
+        return line
 
-                # Prompts and saves the path
-                listPath = input("\nEnter the path of your list: ")
-
-                # Saves the contents of the list
-                scanIPList = os.popen(f"cat {listPath}").read()
-
-                # Displays all the IPs that will be scanned
-                input(f"{scanIPList}\nThe above IPs will be scanned, press enter to continue: ")
-
-                # Asks for the port numbers to be scanned
-                scanPort = input("\nEnter the ports you would like scanned: ")
-                
-                # Interface Input
-                scanInterface = input("\nEnter the network interface name: (Example: eth0, wlan0) ")
-
-                # Iterates through all the IPs in the list and scans
-                with open(f"{listPath}", "r") as file:
-                    for line in file:
-                        ip = line.strip()
-                        if ip:
-                            os.system(f"sudo nmap -sS -p {scanPort} {ip} -e {scanInterface}")
-                break
-
-            # Singular IP Syn Stealth Scan
-            elif (importIP == 'n' or importIP == 'N'):
-                scanIP = input("\nEnter the IP: ")
-                scanPort = input("\nEnter the ports you would like scanned: ")
-                os.system(f"nmap -sV -p {scanPort} {scanIP}")
-                #Print statement for testing
-                #print("os.system(fnmap -A -p ")
-                break
-            
-        # All Scan Conditional
-        elif x == '3':
-            scanIP = input("\nEnter the IP: " )
-            scanPort = input("\nEnter the ports you would like scanned: ")
-            os.system(f"nmap -A -p {scanPort} {scanIP}")
-            #Print statement for testing
-            #print("os.systemnmap -A -p scanPort scanIP")
-            break
-
-        elif x == '4':
-            break
-
+    def do_show(self, arg):
+        "Show options or modules"
+        if arg.strip() == "options":
+            if not self.options:
+                print(colored("[!] No module loaded.", "yellow"))
+                return
+            print(f"\nOptions for module: {self.module}\n")
+            print(f"{'Name':<12} {'Value':<20} Description")
+            print("-" * 50)
+            for key, meta in self.options.items():
+                print(f"{key:<12} {meta['value']:<20} {meta['desc']}")
+        elif arg.strip() == "modules":
+            print("\nModules:\n  nmap\n  nmap_protocol\n")
         else:
-            input(colored("Invalid selection, please press enter.", 'red', attrs=['bold']))
-            continue
+            print("Usage: show options | show modules")
 
-mainMenu()
+    def complete_show(self, text, line, beg, end):
+        return [opt for opt in ['options', 'modules'] if opt.startswith(text)]
+
+    def do_set(self, arg):
+        "Set an option: set OPTION VALUE or SET SCAN"
+        if not self.options:
+            print(colored("[!] Load a module first.", "yellow"))
+            return
+
+        parts = arg.strip().split(None, 1)
+        if len(parts) == 0:
+            print("Usage: set OPTION VALUE")
+            return
+
+        key = parts[0].upper()
+        #scan modes
+        if key == "POOP" and self.module == "nmap":
+            self.show_scan_selector()
+            return
+
+        if len(parts) != 2:
+            print("Usage: set OPTION VALUE")
+            return
+
+        value = parts[1]
+        if key in self.options:
+            self.options[key]["value"] = value
+            print(f"{key} => {value}")
+        else:
+            print(colored(f"[!] Invalid option: {key}", "red"))
+
+    def complete_set(self, text, line, beg, end):
+        return [opt for opt in self.options if opt.startswith(text.upper())]
+
+    def show_scan_selector(self):
+        scan_modes = {
+            "1": ("-sU", "etc"),
+            "2": ("-sT", "etc"),
+            "3": ("-sS", "etc"),
+            "4": ("-sN", "etc"),
+            "5": ("-sF", "etc"),
+            "6": ("-sX", "etc")
+        }
+
+        print(colored("\nScan Types:", "white", attrs=["bold"]))
+        print(f"{'ID':<4} {'Flag':<6} Description")
+        print("-" * 30)
+        for k, (flag, desc) in scan_modes.items():
+            print(f"{k:<4} {flag:<6} {desc}")
+        print()
+
+        choice = input("Select scan type: set scan <number>\n" + colored("enum> ", "green"))
+
+        if not choice.startswith("set scan"):
+            print(colored("[!] Invalid input. Use: set scan <number>", "red"))
+            return
+
+        parts = choice.strip().split()
+        if len(parts) != 3 or parts[1] != "scan" or parts[2] not in scan_modes:
+            print(colored("[!] Invalid scan selection.", "red"))
+            return
+
+        flag = scan_modes[parts[2]][0]
+        self.options["SCAN"]["value"] = flag
+        print(f" Scan mode set to {flag} ({scan_modes[parts[2]][1]})")
+
+    def do_run(self, arg):
+        "Run scan"
+        if not self.options:
+            print(colored("[!] Load a module first.", "yellow"))
+            return
+
+        missing = [k for k, v in self.options.items() if v["required"] and not v["value"]]
+        if missing:
+            print(colored(f"[!] Missing required options: {', '.join(missing)}", "red"))
+            return
+
+        if self.module == "nmap":
+            SCAN = self.options["SCAN"]["value"]
+            rhost = self.options["RHOST"]["value"]
+            ports = self.options["PORTS"]["value"]
+            iface = self.options["INTERFACE"]["value"]
+            ip_list = self.options["IMPORT"]["value"]
+            save = self.options["SAVE"]["value"].lower() in ["true", "yes", "y"]
+
+            if SCAN == "discovery":
+                if not validIp(rhost):
+                    print(colored("[!] Invalid IP/CIDR.", "red"))
+                    return
+                cmd = f"nmap -sn {rhost} -e {iface}"
+                if save:
+                    cmd += " | grep 'Nmap scan' | cut -d ' ' -f 5 > ./discoveryScan.txt"
+                os.system(cmd)
+                if save:
+                    print(colored("[+] Results saved to ./discoveryScan.txt", "cyan"))
+
+            elif SCAN == "stealth":
+                if ip_list:
+                    try:
+                        with open(ip_list) as f:
+                            for line in f:
+                                ip = line.strip()
+                                if ip:
+                                    os.system(f"nmap -sS -p {ports} {ip} -e {iface}")
+                    except Exception as e:
+                        print(colored(f"[!] Error: {e}", "red"))
+                else:
+                    os.system(f"nmap -sS -p {ports} {rhost} -e {iface}")
+
+            elif SCAN == "all":
+                os.system(f"nmap -A -p {ports} {rhost}")
+            else:
+                print(colored("[!] Unknown SCAN: use discovery/stealth/all", "red"))
+
+        elif self.module == "nmap_protocol":
+            rhost = self.options["RHOST"]["value"]
+            proto = self.options["PROTOCOL"]["value"]
+            if proto == "ftp":
+                os.system(f"nmap -sV -p 21 {rhost}")
+            elif proto == "http":
+                os.system(f"nmap -sV -p 80,443 {rhost}")
+            elif proto == "smtp":
+                os.system(f"nmap -sV -p 25 {rhost}")
+            else:
+                print(colored("[!] Unsupported protocol.", "red"))
+
+    def do_clear(self, arg):
+        "Clear screen"
+        os.system("clear")
+
+    def do_exit(self, arg):
+        "Exit shell"
+        print("Goodbye.")
+        return True
+
+    def default(self, line):
+        print(colored(f"[!] Unknown command: {line}", "red"))
+
+if __name__ == "__main__":
+    show_banner()
+    print(colored("\n[1] Nmap", "white"))
+    print(colored("[2] Nmap Protocol Specific", "white"))
+    print(colored("[3] Exit", "white"))
+    print()
+    EnumShell().cmdloop()
--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Developed by The Phantom Phreak & Dana Fletcher
 - Nmap Ping Sweep (Option to save isolated IPs in a list format automatically.)
 - Nmap Service Scan (Option to input isolated IP list automatically)
 - Nmap All Scan (In-Development)
+
+### Note from Dana
+1. Added flowers
+2. Took away all your for loops
+3. TOOK AWAY ALL YOUR FOR LOOPS
+4. TOOK. AWAY. ALL. YOUR. FOR LOOPS.
+
+
